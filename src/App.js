235779d843c06dd08002fd6df11d816dd1830ae3
kculz <munyamakudzai095@gmail.com>
2022-01-29 16:59:04 +0200

    done with app // left with the progressive side

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React,{useState} from "react";
-import {fetchWeather} from './api/fetchWeather'
+import React, { useState } from "react";
+import { fetchWeather } from './api/fetchWeather'
 import './App.css'
 function App() {
 
-  const [query,setQuery] = useState('')
-  
-    const search = async (e)=>{
-      if(e.key === 'Enter'){
-        const data = await fetchWeather(query)
-        console.log(data)
-      }
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const dataa = await fetchWeather(query)
+      console.log(dataa)
+      setWeather(dataa)
+      setQuery('')
     }
+  }
   return (
     <div className="main-container">
-      <input type="text"  
+      <input type="text"
         placeholder="Search..."
         className="search"
         value={query}
-        onChange={(e)=>setQuery(e.target.value)}
+        onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
+      {weather.data && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.data.location.name}</span>
+            <sup>{weather.data.location.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.data.current.temp_c)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img src={weather.data.current.condition.icon} />
+            <p>{weather.data.current.condition.text}</p>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 }
